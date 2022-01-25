1d5517325d80197c5c660cdd300f375c97172556
chenfeng <chenfeng@163.com>
2022-01-25 17:24:31 +0800

    去掉bugly

--- app/src/main/java/com/makeit/baselib/BaseApp.kt
+++ app/src/main/java/com/makeit/baselib/BaseApp.kt
@@ -1,10 +1,7 @@
 package com.makeit.baselib
 
-import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
-import com.tencent.bugly.Bugly
-import com.tencent.tinker.lib.BuildConfig
 
 /**
  * Created by chenfeng on 2020/12/2.
@@ -18,16 +15,10 @@ open class BaseApp : Application() {
     override fun onCreate() {
         super.onCreate()
         app = this
-        Bugly.init(this, buglyAppId, BuildConfig.DEBUG);
     }
-
-
 
     companion object {
 
-        private val buglyAppId = "e10611f1a5"
-
-        @SuppressLint("StaticFieldLeak")
         private lateinit var app: BaseApp
 
         fun getContext(): Context {
